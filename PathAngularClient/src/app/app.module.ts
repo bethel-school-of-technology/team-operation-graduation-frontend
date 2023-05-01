22ca808da66a09779a14478c2a133d60e85c9dd6
Kara Besa <98492902+irshpotato@users.noreply.github.com>
2023-04-30 21:13:12 -0400

    PathAngularClient

--- PathAngularClient/src/app/app.module.ts
+++ PathAngularClient/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectListComponent,
+    NewProjectComponent,
+    LogInComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
