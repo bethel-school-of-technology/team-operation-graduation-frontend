b0bb6abca633c2e6c7d30a21ceb76e394e68e017
TKKitt <tylerkitt4@gmail.com>
2023-05-01 12:27:41 -0500

    commit 2 cleanup

---
+++ PathAngularClient/src/app/app-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import { LoginComponent } from './login/login.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'workspace',
+    pathMatch: 'full'
+  },
+  {
+    path: 'workspace',
+    loadChildren: () => import('./workspace/workspace.module').then(m => m.WorkspaceModule)
+  },
+  {
+    path: 'register',
+    component: CreateAccountComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ PathAngularClient/src/app/create-account/create-account.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+<<<<<<< HEAD
+import { User } from '../Models/user.model';
+=======
+>>>>>>> 22ca808da66a09779a14478c2a133d60e85c9dd6
+
+@Component({
+  selector: 'app-create-account',
+  templateUrl: './create-account.component.html',
+  styleUrls: ['./create-account.component.css']
+})
+export class CreateAccountComponent {
+<<<<<<< HEAD
+  user = new User();
+
+  constructor() { }
+
+  register() {
+    console.log("User registered");
+  }
+=======
+
+>>>>>>> 22ca808da66a09779a14478c2a133d60e85c9dd6
+}
