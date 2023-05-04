3fd596cfa3cbd1d4c910bf52f6be1df949bb3d6b
Kara Besa <98492902+irshpotato@users.noreply.github.com>
2023-05-04 19:13:51 -0400

    testing

--- PathAngularClient/src/app/app.module.ts
+++ PathAngularClient/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,12 +22,9 @@ import { HttpClientModule } from '@angular/common/http'
     AppRoutingModule,
     FormsModule,
     WorkspaceModule,
-<<<<<<< HEAD
     ReactiveFormsModule,
     HttpClientModule,
-=======
-    HttpClientModule
->>>>>>> origin/main
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
